63c7a4cd5c547833d0b9f513df3603ce35438366
Rakesh <rakeshrkz2101@gmail.com>
2025-03-25 19:12:38 +0530

    Update experience.js

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,13 +1,13 @@
 export const experience = [{
-    year: "2021(9 Months)",
-    Company_name: "Dark Diamond Technologies Pvt, Ltd",
-    job_desc: "As a Web Developer (Fresher), I worked with HTML, CSS, and React (Basics) to develop and maintain responsive web pages. My role involved assisting in front-end development, ensuring visually appealing and user-friendly designs while learning best practices for UI/UX. I collaborated with senior developers to enhance website performance, worked on bug fixes, and optimized website structures to improve usability and efficiency."
+    year: "2023(Oct) - Present",
+    Company_name: "BGSW (Bosch Global Software Technologies)",
+    job_desc: "I specialize in building and maintaining full-stack applications using MongoDB, Express.js, React, and Node.js. My role extends beyond traditional development, integrating DevOps practices to streamline deployment and enhance system reliability.I work with OpenShift for container orchestration, ensuring scalable and efficient cloud-native applications. Additionally, I utilize Jenkins for CI/CD automation and SonarQube for code quality and security analysis. My responsibilities involve optimizing workflows, improving deployment pipelines, and collaborating with cross-functional teams to deliver high-performance applications with robust security and maintainability."
 }, {
-    year: "2021 - 2023 (2Years 1Month)",
+    year: "2021(Sep) - 2023(Sep) (2Years 1Month)",
     Company_name: "SRJ Infognana Solution Pvt, Ltd",
     job_desc: "As a MERN Stack Developer, I was responsible for developing and maintaining full-stack web applications using MongoDB, Express.js, React, and Node.js. My role involved building responsive and scalable user interfaces, developing RESTful APIs, and handling database operations. I worked closely with cross-functional teams to deliver efficient and high-performing applications. Additionally, I began exploring AWS services, gaining hands-on experience in cloud computing, deployment, and infrastructure management, which helped enhance the scalability and reliability of the applications."
 }, {
-    year: "2023 - Present",
-    Company_name: "BGSW (Bosch Global Software Technologies)",
-    job_desc: "I specialize in building and maintaining full-stack applications using MongoDB, Express.js, React, and Node.js. My role extends beyond traditional development, integrating DevOps practices to streamline deployment and enhance system reliability.I work with OpenShift for container orchestration, ensuring scalable and efficient cloud-native applications. Additionally, I utilize Jenkins for CI/CD automation and SonarQube for code quality and security analysis. My responsibilities involve optimizing workflows, improving deployment pipelines, and collaborating with cross-functional teams to deliver high-performance applications with robust security and maintainability."
+    year: "2021(Jan) - 2021(Sep) (9 Months)",
+    Company_name: "Dark Diamond Technologies Pvt, Ltd",
+    job_desc: "As a Web Developer (Fresher), I worked with HTML, CSS, and React (Basics) to develop and maintain responsive web pages. My role involved assisting in front-end development, ensuring visually appealing and user-friendly designs while learning best practices for UI/UX. I collaborated with senior developers to enhance website performance, worked on bug fixes, and optimized website structures to improve usability and efficiency."
 }]
